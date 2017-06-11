1826baf66caaf107aa59676731bad88cea8b01ce
JacksonTxl <172004133@qq.com>
2017-06-11 16:16:34 +0800

    add test file

---
+++ component/test/build/config.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  port: '4002',
+  title: 'test',
+  vendor: [
+    'vue',
+    'vue-router'
+  ],
+  cssModules: false
+}
---
+++ component/test/src/router/Home/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <el-button type="primary" @click.native="hello">primary</el-button>
+    <hello></hello>
+  </main>
+</template>
+
+<script>
+  import { Button, Message } from 'element-ui'
+  import { firstName, lastName, year, multiply, Norm } from 'simba_demo'
+
+  import Hello from 'components/Hello'
+
+  export default {
+    components: {
+      ElButton: Button,
+      Hello
+    },
+    methods: {
+      hello () {
+        Message('Hello Vue'+ firstName + lastName + year + multiply(1,2));
+        var m = new Norm(1234,456) ;
+        console.log(m.testFun(12345,7896));
+        console.log(m.toString());
+      }
+    }
+  }
+</script>
